a0e86c7af720921e9b67c3abb10c6ffc3c6dbc65
CC <87725344+CC-bit87@users.noreply.github.com>
2023-08-01 14:49:50 +0200

    Add files via upload

---
+++ quiz-game-start/data.py
@@ -0,0 +1,23 @@
+question_data = [
+    {"question": "Nutella is produced by the German company Ferrero.", "correct_answer": "False",
+     "incorrect_answers": ["True"]},
+    {"question": "A scientific study on peanuts in bars found traces of over 100 unique specimens of urine.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "Bulls are attracted to the color_extract red.", "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "The Sun rises from the North.", "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"question": "You can legally drink alcohol while driving in Mississippi.", "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+    {"question": "Club 27; is a term used to refer to a list of famous actors, musicians, and artists "
+                 "who died at the age of 27.", "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "Dihydrogen Monoxide was banned due to health risks after being discovered in 1983 inside swimming "
+                 "pools and drinking water.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "Scotland voted to become an independent country during the referendum from September 2014.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"question": "A pasodoble is a type of Italian pasta sauce.", "correct_answer": "False",
+     "incorrect_answers": ["True"]}, {"question": "The mitochondria is the powerhouse of the cell.",
+                                      "correct_answer": "True", "incorrect_answers": ["False"]}]
